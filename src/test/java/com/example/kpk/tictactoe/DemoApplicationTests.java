e1bf992ab92731530f8c5dc149ab975e20f4c1ef
krasimiraMilkova <krasimira_milkova@outlook.com>
2020-05-17 19:41:01 +0300

    Initial spring boot project

---
+++ src/test/java/com/example/kpk/tictactoe/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kpk.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
